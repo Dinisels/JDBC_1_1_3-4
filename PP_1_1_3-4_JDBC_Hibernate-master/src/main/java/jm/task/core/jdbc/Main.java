7f071f3a51e0554eabf5e1ee35cf0678bdfe343b
Dinicels <asitack15@gmail.com>
2025-07-14 11:32:39 +0300

    remove_dop_check_exists

--- PP_1_1_3-4_JDBC_Hibernate-master/src/main/java/jm/task/core/jdbc/Main.java
+++ PP_1_1_3-4_JDBC_Hibernate-master/src/main/java/jm/task/core/jdbc/Main.java
@@ -10,32 +10,32 @@ public class Main {
     public static void main(String[] args) {
 
         UserService userService = new UserServiceImpl();
-
+//
         userService.createUsersTable(); // Создание таблицы User(ов)
-
-        //Добавление 4 User(ов) в таблицу с данными на свой выбор. После каждого добавления должен быть вывод в консоль (User с именем — name добавлен в базу данных)
-
+//
+//        //Добавление 4 User(ов) в таблицу с данными на свой выбор. После каждого добавления должен быть вывод в консоль (User с именем — name добавлен в базу данных)
+//
         userService.saveUser("Ivan", "Ivanovich", (byte) 12);
         userService.saveUser("Petr", "Petrovich", (byte) 24);
         userService.saveUser("Senya", "Senovalov", (byte) 36);
         userService.saveUser("Miha", "Mihanov", (byte) 32);
         userService.saveUser("Rebs", "Rebsovich", (byte) 89);
-
-
-        // Удаление пользователя по id, в здании не сказано чтобы был в мейне
+//
+//
+//        // Удаление пользователя по id, в здании не сказано чтобы был в мейне
         //userService.removeUserById(2);
-
-        // Получение всех User из базы и вывод в консоль (должен быть переопределен toString в классе User)
+//
+//        // Получение всех User из базы и вывод в консоль (должен быть переопределен toString в классе User)
         System.out.println(userService.getAllUsers());
-
-        //Очистка таблицы User(ов)
+//
+//        //Очистка таблицы User(ов)
         userService.cleanUsersTable();
-
-        // Получение всех User из базы и вывод в консоль - оставил чтобы было видно что очистилась таблица
+//
+//         Получение всех User из базы и вывод в консоль - оставил чтобы было видно что очистилась таблица
         System.out.println(userService.getAllUsers());
 
         //Удаление таблицы
-       //userService.dropUsersTable();
+       userService.dropUsersTable();
 
     }
 }
